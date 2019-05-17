afcf51b94be2e114042e75db485272060928893e
Ron De Benedetti <RonDeBen@gmail.com>
2019-05-17 13:03:17 -0500

    now sends avatar and nickname to localhost api

--- Assets/Scripts/Layouts/MainMenuLayout.cs
+++ Assets/Scripts/Layouts/MainMenuLayout.cs
@@ -9,11 +9,17 @@ public class MainMenuLayout : MonoBehaviour {
 	public VerticalLayoutGroup VLG;
 	public float 	leftPercentage, rightPercentage, topPercentage, bottomPercentage, spacingPercentage,
 					logoHeightPercentage, topTextHeightPercentage, avatarHeightPercentage, nicknameHeightPercentage, findGameHeightPercentage;
-	
+	public Text nicknameText;
+	public Image avatarImage;
+	private Texture2D avatarTex;
+	public Texture2D dummy;
 	private Rect canvasRect;
 
 	// Use this for initialization
 	void Start () {
+		#if UNITY_EDITOR || UNITY_STANDALONE
+			avatarImage.sprite = Sprite.Create(dummy, new Rect(0.0f, 0.0f, dummy.width, dummy.height), new Vector2(0.5f, 0.5f), 100.0f);;
+		#endif
 		Rect canvasRect = gameObject.GetComponent<RectTransform>().rect;
 		float height = canvasRect.height;
 		//left, right, top, bottom
@@ -30,5 +36,38 @@ public class MainMenuLayout : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void OnAvatarButtonClicked(){
+		if(NativeGallery.IsMediaPickerBusy()){
+			return;
+		}
+
+		PickImage(512);
+	}
+
+	public void OnFindGameButtonClicked(){
+		RequestManager.instance.SendUserStuff(nicknameText.text, avatarTex);
+	}
+
+	private void PickImage( int maxSize )
+	{
+		NativeGallery.Permission permission = NativeGallery.GetImageFromGallery( ( path ) =>
+		{
+			Debug.Log( "Image path: " + path );
+			if( path != null )
+			{
+				// Create Texture from selected image
+				avatarTex = NativeGallery.LoadImageAtPath( path, maxSize );
+				if( avatarTex == null )
+				{
+					Debug.Log( "Couldn't load sprite from " + path );
+					return;
+				}
+
+				avatarImage.sprite = Sprite.Create(avatarTex, new Rect(0.0f, 0.0f, avatarTex.width, avatarTex.height), new Vector2(0.5f, 0.5f), 100.0f);
+			}
+		}, "Select a PNG image", "image/png", maxSize );
+		Debug.Log( "Permission result: " + permission );
 	}
 }
---
+++ Assets/Scripts/RequestManager.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class RequestManager : MonoBehaviour {
+
+	public static RequestManager instance;
+	public bool isLocalGame;
+	private string url;
+	private Sprite userSprite;
+
+	void Awake(){
+		if(instance != null){
+			GameObject.Destroy(instance);
+		}
+		instance = this;
+		#if UNITY_EDITOR || UNITY_STANDALONE
+			if(isLocalGame){
+				url = "http://localhost:3000";
+			}else{
+				url = "http://104.236.242.234";
+			}
+		#elif UNITY_IPHONE || UNITY_ANDROID
+			url = "http://104.236.242.234";
+		#endif
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// IEnumerator GetRequest(string uri){
+	//     bool requestFinished = false;
+	//     bool requestErrorOccurred = false;
+
+	//     UnityWebRequest request = UnityWebRequest.Get(uri);
+	//     yield return request.Send();
+
+	//     requestFinished = true;
+	//     if (request.isError){
+	//         Debug.Log("Something went wrong, and returned error: " + request.error);
+	//         requestErrorOccurred = true;
+	//     }
+	//     else{
+	//         // Show results as text
+
+	//         if (request.responseCode == 200){
+	//         }
+	//         else{
+	//             Debug.Log("Request failed (status:" + request.responseCode + ")");
+	//             requestErrorOccurred = true;
+	//         }
+	//         if (!requestErrorOccurred){
+	//             yield return null;
+	//             // process results
+	//         }
+	//     }
+	// }
+
+	// IEnumerator PostRequest(string uri, string bodyJsonString){
+	//     var request = new UnityWebRequest(uri, "POST");
+	//     byte[] bodyRaw = new System.Text.UTF8Encoding().GetBytes(bodyJsonString);
+	//     request.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
+	//     request.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
+	//     request.SetRequestHeader("Content-Type", "application/json");
+
+	//     yield return request.Send();
+	//     Debug.Log("Response: " + request.downloadHandler.text);
+	// }
+
+	IEnumerator UploadUserStuff(Texture2D tex, string nickname, string uri)
+    {
+        // Encode texture into PNG
+        byte[] bytes = tex.EncodeToPNG();
+
+        //create sprite from tex, and save to userSprite
+        userSprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100.0f);;
+
+        // Create a Web Form
+        WWWForm form = new WWWForm();
+        form.AddField("nickname", nickname);
+        form.AddBinaryData("avatar", bytes, "avatar.png", "image/png");
+
+        // Upload to a cgi script
+        using (var w = UnityWebRequest.Post(uri, form))
+        {
+            yield return w.Send();
+            if (w.isError) {
+                print(w.error);
+            }
+            else {
+                print("Finished Uploading Screenshot");
+            }
+        }
+    }
+
+    public void SendUserStuff(string nickname, Texture2D tex){
+		string uri = url + "/users/login.json";
+		StartCoroutine(UploadUserStuff(tex, nickname, uri));
+	}
+}
